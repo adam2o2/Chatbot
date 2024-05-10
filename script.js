eb01ed379747709558a2690966b10dce3673a5ca
adam2o2 <78441732+adam2o2@users.noreply.github.com>
2024-05-10 15:15:29 -0400

    bot changes

---
+++ script.js
@@ -0,0 +1,42 @@
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+const chatbox = document.querySelector(".chatbox");
+
+let userMessage;
+const API_KEY = "";
+
+const createChatLi = (message, className) => {
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+    let chatContent = className === "outgoing" ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`
+    chatLi.innerHTML = chatContent;
+    return chatLi;
+}
+const generateResponse = () => {
+    const API_URL = "https://api.openai.com/v1/chat/completions"
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: ""
+        })
+    }
+}
+
+const handelChat = () => {
+    userMessage = chatInput.ariaValueMax.trim();
+    if(!userMessage) return;
+
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+
+    setTimeout(() =>{
+        chatbox.appendChild(createChatLi("Thinking...", "incoming"));
+        generateResponse();
+    }, 600);
+}
+
+sendChatBtn.addEventListener("click", handelChat)
