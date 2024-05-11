f802f0371106eba8e87e4b8eccc29dd1a3ea8c0e
adam2o2 <78441732+adam2o2@users.noreply.github.com>
2024-05-11 17:40:18 -0400

    thanks abi

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const sendChatBtn = document.querySelector(".chat-input span");
 const chatbox = document.querySelector(".chatbox");
 
 let userMessage;
-const API_KEY = "sk-proj-7ovhJW5hUxjkf9AEePoAT3BlbkFJOyco1uHY2jms61PIxfBK";
+const API_KEY = "";
 
 const createChatLi = (message, className) => {
     const chatLi = document.createElement("li");
